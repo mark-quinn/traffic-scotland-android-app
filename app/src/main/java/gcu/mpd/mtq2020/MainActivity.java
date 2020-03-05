ad15fee69c06773929a6d9e13fe215c9d09db06b
mark-quinn <markquinn97@hotmail.com>
2020-03-05 10:04:07 +0000

    added simple dropdown spinner

--- app/src/main/java/gcu/mpd/mtq2020/MainActivity.java
+++ app/src/main/java/gcu/mpd/mtq2020/MainActivity.java
@@ -1,6 +1,8 @@
 package gcu.mpd.mtq2020;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.spinnerEvent);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.event_choices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         new TrafficInformation().getCurrentIncidents();
     }
 }

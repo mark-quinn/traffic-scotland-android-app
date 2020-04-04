1e50e778a6bfaab21f5f09ef539de79da78502b9
mark-quinn <markquinn97@hotmail.com>
2020-04-04 16:26:12 +0100

    add search traffic event

---
+++ app/src/main/java/gcu/mpd/mtq2020/SearchTraffic.java
@@ -0,0 +1,56 @@
+package gcu.mpd.mtq2020;
+
+import android.app.DatePickerDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.DatePicker;
+import android.widget.ListView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+
+public class SearchTraffic extends AppCompatActivity {
+    private static final String TAG = "SearchTraffic";
+    private DatePickerDialog picker;
+    private EditText eText;
+    private ListView listEvents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_traffic);
+        listEvents = findViewById(R.id.eventListView);
+        eText = findViewById(R.id.eventDate);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+
+                picker = new DatePickerDialog(SearchTraffic.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+    }
+}

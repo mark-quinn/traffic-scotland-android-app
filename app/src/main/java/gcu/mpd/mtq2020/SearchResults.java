5ce11c451f09df93f4b39ca565a9371c95b14987
mark-quinn <markquinn97@hotmail.com>
2020-04-05 11:50:16 +0100

    display events in search result page

--- app/src/main/java/gcu/mpd/mtq2020/SearchResults.java
+++ app/src/main/java/gcu/mpd/mtq2020/SearchResults.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +17,7 @@ public class SearchResults extends AppCompatActivity implements AsyncTaskListene
     private Date date;
     private ListView listEvents;
     private List<Event> resultEvents;
+    private TextView tvw;
 
     public SearchResults() {
         resultEvents = new ArrayList<>();
@@ -25,6 +27,8 @@ public class SearchResults extends AppCompatActivity implements AsyncTaskListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        listEvents = findViewById(R.id.eventListView);
+        tvw = findViewById(R.id.totalEvents);
         Intent intent = getIntent();
         date = (Date)intent.getSerializableExtra("DATE");
         Log.i(TAG, "onCreate: Date passed " + date);
@@ -34,9 +38,13 @@ public class SearchResults extends AppCompatActivity implements AsyncTaskListene
 
     @Override
     public void newEvents(ArrayList<Event> events) {
-        Log.d(TAG, "newEvents: New events" + events);
+        Log.d(TAG, "newEvents: New events count" + events.size());
         filterByCriteria(date, events);
-        Log.d(TAG, "newEvents: Result events" + resultEvents);
+        Log.d(TAG, "newEvents: Filter result events" + resultEvents.size());
+        tvw.setText("Found " + resultEvents.size() + " events matching the search criteria");
+        FeedAdapter feedAdapter = new FeedAdapter(
+                SearchResults.this, R.layout.traffic_event, resultEvents);
+        listEvents.setAdapter(feedAdapter);
     }
 
     private void filterByCriteria(Date date, List<Event> events) {

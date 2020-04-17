0d2fe2c0ced229b76da4e8601c6fe6860c0a4a61
mark-quinn <markquinn97@hotmail.com>
2020-04-17 15:29:05 +0100

    WIP: display events in search results

--- app/src/main/java/gcu/mpd/mtq2020/ui/main/PlaceholderFragment.java
+++ app/src/main/java/gcu/mpd/mtq2020/ui/main/PlaceholderFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,11 +14,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import gcu.mpd.mtq2020.Event;
 import gcu.mpd.mtq2020.EventType;
+import gcu.mpd.mtq2020.FeedAdapter;
 import gcu.mpd.mtq2020.R;
 import gcu.mpd.mtq2020.Repository;
+import gcu.mpd.mtq2020.SearchResults;
 import gcu.mpd.mtq2020.TrafficURL;
 
 /**
@@ -28,6 +32,8 @@ public class PlaceholderFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
+    private ListView listEvents;
+
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -56,6 +62,8 @@ public class PlaceholderFragment extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_results, container, false);
         final TextView textView = root.findViewById(R.id.section_label);
+        listEvents = root.findViewById(R.id.eventListView);
+
         pageViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -66,7 +74,9 @@ public class PlaceholderFragment extends Fragment {
         pageViewModel.getEvents().observe(this, new Observer<ArrayList<Event>>() {
             @Override
             public void onChanged(ArrayList<Event> events) {
-                System.out.println("New events");
+                FeedAdapter feedAdapter = new FeedAdapter(
+                        Objects.requireNonNull(getContext()), R.layout.traffic_event, events);
+                listEvents.setAdapter(feedAdapter);
             }
         });
         return root;

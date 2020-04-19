9fa24aaf55cba3b5b222ce84ca2e8b74d44f6bf5
mark-quinn <markquinn97@hotmail.com>
2020-04-19 20:59:32 +0100

    initial work for routes page

--- app/src/main/java/gcu/mpd/mtq2020/ui/routes/RoutesFragment.java
+++ app/src/main/java/gcu/mpd/mtq2020/ui/routes/RoutesFragment.java
@@ -1,25 +1,89 @@
 package gcu.mpd.mtq2020.ui.routes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
 
 import gcu.mpd.mtq2020.R;
 
 public class RoutesFragment extends Fragment {
 
     private RoutesViewModel routesViewModel;
+    private static final String TAG = "RoutesFragment";
+    private PlacesClient placesClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getContext(), getString(R.string.google_maps_key), Locale.UK);
+        }
+
+        placesClient = Places.createClient(Objects.requireNonNull(getContext()));
+
         routesViewModel =
                 ViewModelProviders.of(this).get(RoutesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_routes, container, false);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment_from);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        autocompleteFragment.setCountry("UK");
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId() + ", " + place.getLatLng());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
+        AutocompleteSupportFragment autocompleteFragment2 = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment_to);
+
+        autocompleteFragment2.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        autocompleteFragment2.setCountry("UK");
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId() + ", " + place.getLatLng());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
         return root;
     }
 }
--- app/src/main/java/gcu/mpd/mtq2020/ui/routes/RoutesViewModel.java
+++ app/src/main/java/gcu/mpd/mtq2020/ui/routes/RoutesViewModel.java
@@ -1,19 +1,9 @@
 package gcu.mpd.mtq2020.ui.routes;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class RoutesViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
     public RoutesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
     }
 }
